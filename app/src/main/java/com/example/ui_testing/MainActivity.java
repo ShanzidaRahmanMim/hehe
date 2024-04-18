874e623b821ccda1213e9a1e9fded2d2924d0ff4
ShanzidaRahmanMim <shanzidamim33@gmail.com>
2024-04-18 09:03:23 +0600

    hehe

---
+++ app/src/main/java/com/example/ui_testing/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.ui_testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+Button button1, button2,button3;
+TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1=findViewById(R.id.btn1);
+        button2=findViewById(R.id.btn2);
+        button3=findViewById(R.id.btn3);
+        textView=findViewById(R.id.text);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("This is red text");
+                textView.setTextColor(getResources().getColor(R.color.red));
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("This is green text");
+                textView.setTextColor(getResources().getColor(R.color.green));
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
